0a8844c5cc99aa65e732dc98ab77cd6e179ea0ea
Giancarlo <ggarciadeleon@csustan.edu>
2022-01-08 02:28:58 -0800

    prepared app for dockerization

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link"
 import styles from '../styles/Home.module.css'
 import StairCase from "../public/images/Stairs.jpeg"
 import GithubIcon from "../public/images/github-fill.svg"
 import LinkedInIcon from "../public/images/linkedin-fill.svg"
-
 
 export default function Home() {
   return (
@@ -26,27 +24,27 @@ export default function Home() {
               <div className={styles.smContainer}>
                 <p>Full Stack Developer</p>
                 <div className={styles.imgContainer}>
-                  <Link href="https://linkedin.com/in/giancarlo-garcia/" passHref alt="LinkedIn link to Giancarlo's Page">
-                    <Image className={styles.smImage} src={LinkedInIcon} alt="LinkedIn Icon" width={50} height={50}/>
+                  <Link href="https://linkedin.com/in/giancarlo-garcia/"  alt="LinkedIn link to Giancarlo&apos;s Page" passHref>
+                    <Image className={styles.smImage} src={LinkedInIcon} alt="LinkedIn Icon" width={50} height={50} />
                   </Link>
                   
-                  <Link href="https://github.com/ggiande" alt="Github link to Giancarlo's Page">
+                  <Link href="https://github.com/ggiande" alt="Github link to Giancarlo&apos;s Page" passHref>
                     <Image className={styles.smImage} src={GithubIcon} alt="Github Icon" width={50} height={50}/>
                   </Link>
                 </div>
               </div>
           </section>
 
-          <div className={styles.hr}/>
-              <p>
-                <ul className={styles.list}>
-                  <li>Hello there, 👋 I am currently pursuing a Bachelor's degree in Computer Science</li>
-                  <li>💻 I'm a Computer Science major at California State University Stanislaus.</li>
-                  <li>💼 In the past I have worked as a Software Engineering Academic Intern at JPMorgan Chase & Co.</li>
-                  <li>💬 Feel free to talk to me about paving the way for students to achieve success through new opportunities and being as inclusive as possible.</li>
-                  <li>🔨 I'm currently working on a handful of side projects and partaking in new initiatives.</li>
-                </ul>
-              </p>
+          <div className={styles.hr} />
+          <p>
+            <ul className={styles.list}>
+              <li>Hello there, 👋 I am currently pursuing a Bachelor&apos;s degree in Computer Science</li>
+              <li>💻 I&apos;m a Computer Science major at California State University Stanislaus.</li>
+              <li>💼 In the past I have worked as a Software Engineering Academic Intern at JPMorgan Chase&amp;Co.</li>
+              <li>💬 Feel free to talk to me about paving the way for students to achieve success through new opportunities and being as inclusive as possible.</li>
+              <li>🔨 I&apos;m currently working on a handful of side projects and partaking in new initiatives.</li>
+            </ul>
+          </p>
       </main>
     </>
   )
